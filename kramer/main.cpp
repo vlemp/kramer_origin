6c266c93b259dfc765e8dfedf2202e73194ca837
vlemp <87314082+vlemp@users.noreply.github.com>
2021-07-21 12:16:36 +0300

    работа с таблицей

--- kramer/main.cpp
+++ kramer/main.cpp
@@ -1,6 +1,14 @@
 #include "mainwindow.h"
+#include "ui_mainwindow.h"
 
 #include <QApplication>
+
+
+#include <stdlib.h>
+#include <QRandomGenerator>
+#include <QTextStream>
+#include <QTableWidget>
+#include <QMessageBox>
 
 int main(int argc, char *argv[])
 {
@@ -9,3 +17,31 @@ int main(int argc, char *argv[])
     w.show();
     return a.exec();
 }
+
+void MainWindow::on_GenMatrix_clicked() //проверка работы таблицы с кнопкой и значениями //lerch start
+{
+//(данные таблицы не совпадают с данными программы, стадия проверки)
+    //ui->tableWidget->setMaximumSize(tableWidget); проверка подгонки таблицы по размерам
+    int k = qrand() % 10;
+    ui->tableWidget->setRowCount(n);
+    ui->tableWidget->setColumnCount(n);
+
+    for(int i=0; i< ui->tableWidget->rowCount(); i++){
+        for(int j=0; j< ui->tableWidget->columnCount(); j++){
+            k = qrand() % 10;
+            QTableWidgetItem *itm = new QTableWidgetItem(tr("%1").arg(k));
+
+            ui->tableWidget->setItem(i,j,itm);
+        }
+    }
+    for (int i=0; i< ui->tableWidget->rowCount(); ++i)
+    {
+        for(int j=0; j< ui->tableWidget->columnCount(); j++){
+
+            qDebug() << ui-> tableWidget->item(i, 0)->text();
+
+        }
+        qDebug() << endl;
+    }
+
+} //lerch finish
